c9f5fe0fccd18e1a8d8a8533a0b353ca31b35ad1
sujungeee <113659619+sujungeee@users.noreply.github.com>
2024-06-15 13:57:37 +0900

    [update] 2회차- (다이나믹 프로그래밍) 71 LIS 길이 계산하기.py

--- 2회차/(다이나믹 프로그래밍) 71 LIS 길이 계산하기.py
+++ 2회차/(다이나믹 프로그래밍) 71 LIS 길이 계산하기.py
@@ -1,13 +1,4 @@
-# 이렇게 해도 될 것 같은데,,
 def solution(nums):
-    cnt= 1
-    for i in range(1, len(nums)):
-        if nums[i] > nums[i-1]:
-            cnt+=1
-    return cnt
-
-# solution2:
-def solution2(nums):
     dp= [1 for _ in range(len(nums))]
     for i in range(1, len(nums)):
         for j in range(i):
@@ -16,6 +7,4 @@ def solution2(nums):
     return max(dp)
 
 print(solution([1, 4, 2, 3, 1, 5, 7, 3]))
-print(solution2([1, 4, 2, 3, 1, 5, 7, 3]))
 print(solution([3, 2, 1]))
-print(solution2([3, 2, 1]))
